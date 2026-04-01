fc87059725aa8bc0de8cbcd1abeaf6b187cde043
Raj Patel <rajpatel.260303@gmail.com>
2026-04-01 23:51:31 +0530

    Saved the submissions of a problem in database

--- backend/src/controllers/executeCode.controller.js
+++ backend/src/controllers/executeCode.controller.js
@@ -1,3 +1,4 @@
+import { db } from "../libs/db.libs.js";
 import { pollBatchResults, submitBatch } from "../libs/judge0.lib.js";
 import { BadRequestError, InternalServerError } from "../utils/ApiError.utils.js";
 import { ApiResponse } from "../utils/ApiReponse.utils.js";
@@ -20,8 +21,58 @@ const executeCode = async(req,res,next)=>{
         const tokens = submitResponse.map((res)=> res.token);
 
         const results = await pollBatchResults(tokens);
-        console.log("Result----");
-        console.log(results);
+        // console.log("Result----");
+        // console.log(results);
+
+
+        let allPassed = true;
+        const detailedResults = results.map((result,i)=>{
+            const stdout = result.stdout?.trim();
+            const expected_output = expected_outputs[i]?.trim();
+
+            const passed = stdout === expected_output;
+            // console.log(`TestCase #${i+1}`);
+            // console.log(`Input ${stdin[i]}`);
+            // console.log(`Expected Output for test case #${i+1} is ${expected_output}`)
+            // console.log(`Actual Output for test case #${i+1} is ${stdout}`)
+            // console.log(`Matched: ${passed} for Test Case #${i+1}`);
+
+            return {
+                testCase:i+1,
+                passed,
+                stdout,
+                expected:expected_output,
+                stderr:result.stderr || null,
+                compile_output: result.compile_output || null,
+                status:result.status.description,
+                memory:result.memory?`${result.memory}KB`:undefined,
+                time:result.time?`${result.time}s`:undefined
+            }
+        });
+
+        console.log(detailedResults);
+
+        const submission = await db.submission.create({
+            data:{
+                userId,
+                problemId,
+                sourceCode:source_code,
+                language_id:getLanguage(language_id),
+                stdin:stdin.join("\n"),
+                stdout:JSON.stringify(detailedResults.map((r)=>r.stdout)),
+                stderr:detailedResults.some((r)=> r.stderr? JSON.stringify(detailedResults.map((r)=>r.stderr)):null),
+                compileOutput:detailedResults.some((r)=>r.compile_output? JSON.stringify(detailedResults.map((r)=>r.compile_output)):null),
+                status:allPassed?"Accepted":"Wrong Answer",
+                memory: detailedResults.some((r)=> r.memory?JSON.stringify(detailedResults.map((r)=>r.memory)):null),
+                time:detailedResults.some((r)=> r.time?JSON.stringify(detailedResults.map((r)=>r.time)):null)
+            }
+        });
+
+        
+
+
+
+
         return res.status(200).json(ApiResponse.success(null,"Code executed successfully"));
     } catch(err){
         next(err);
